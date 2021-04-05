77de24961dddffece2bd1ee1d96ab67239f84125
17-serch <iag.guaman@yavirac.edu.ec>
2021-04-05 16:59:55 -0500

    cambio estilo login

--- resources/views/crear_cuenta.blade.php
+++ resources/views/crear_cuenta.blade.php
@@ -11,51 +11,51 @@
         <link rel="stylesheet" href="{{ asset('css/app.css')}}">
         
     </head>
-<body class="bg-blue-200">
+<body class="bg-grey-100 grid grid-rows-1">
 	<div class="bg-grey-lighter min-h-screen flex flex-col">
             <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="bg-green-400 px-7 py-9 rounded shadow-md w-full">
-                    <h1 class="mb-8 text-5xl text-center font-serif md:georgia text-gray-600">CREAR CUENTA</h1>
+                <div class="bg-white px-7 py-9 rounded shadow-md w-full">
+                    <h1 class="mb-8 text-5xl text-center font-serif md:georgia text-black">CREAR CUENTA</h1>
 
                     <img src="https://www.gobiernoabierto.ec/wp-content/uploads/2020/01/Logo-Esquel-color.png" alt="logo" class="fill-current h-100 w-100 mr-2" width="250" height="250">
 
-                    <label class="uppercase tracking-wide text-indigo-600 font-serif mb-10 w-50 text-xl"
+                    <label class="uppercase tracking-wide text-black font-serif mb-10 w-50 text-xl"
                         for="usuario"> Usuario 
                     </label>
                     
                     <input 
                         type="text"
-                        class="block border border-white  w-full p-3 rounded mb-4 text-blue-800"
+                        class="block border border-green-900  w-full p-3 rounded mb-4 text-blue-800"
                         name="usuario"
                         placeholder=""/>
 
-                    <label class="uppercase tracking-wide text-indigo-600 font-serif mb-10 w-50 text-xl" 
+                    <label class="uppercase tracking-wide text-black font-serif mb-10 w-50 text-xl" 
                         for="contraseña"> Contraseña 
                     </label>
 
                     <input 
                         type="password"
-                        class="block border border-white w-full p-3 rounded mb-4 text-blue-800"
+                        class="block border border-green-900 w-full p-3 rounded mb-4 text-blue-800"
                         name="password"
                         placeholder=""/>
                     
                       
 
-                    <label class="uppercase tracking-wide text-indigo-600 font-serif mb-10 w-50 text-xl" 
+                    <label class="uppercase tracking-wide text-black font-serif mb-10 w-50 text-xl" 
                         for="contraseña"> Confirmar contraseña
                     </label>
 
                     <input 
                         type="password"
-                        class="block border border-white w-full p-3 rounded mb-4 text-blue-800"
+                        class="block border border-green-900 w-full p-3 rounded mb-4 text-blue-800"
                         name="password"
                         placeholder=""/>
 
                     <a href="#">   
                     
-                    <div class="text-center text-sm text-grey-100 mt-3">
-                    <button class="bg-blue hover:bg-blue-dark text-gray-100 font-bold py-2 px-4 rounded text-2xl">
-                        crear  
+                    <div class="text-center text-sm text-gray-700 mt-3">
+                    <button class="bg-blue hover:bg-blue-dark text-gray-700 font-bold py-2 px-4 rounded text-2xl">
+                    Crear
                     </button>
                     </div>
                         
