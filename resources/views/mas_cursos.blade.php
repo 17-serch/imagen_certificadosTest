14485b414eff46c6cea25427e5860d540ee4dde4
JhonJSimbanaB <jjb.simbana@yavirac.edu.ec>
2021-04-07 14:36:00 -0500

    navegacio

--- resources/views/mas_cursos.blade.php
+++ resources/views/mas_cursos.blade.php
@@ -18,7 +18,7 @@
         margin-top: 50px;
         margin-bottom: 50px;
         width: 850px;
-        height: 900px;
+        height: auto;
     }
 </style>
 
@@ -26,10 +26,11 @@
 <body>
 
     <!-- Barra de navegacion -->
-    <nav class="bg-gray-400 p-6">
+    <nav class="bg-gray-300 p-6">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
                     <!-- Botón de menú móvil-->
                     <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Abrir menú principal</span>
@@ -37,7 +38,6 @@
                         <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
-
                         <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
@@ -46,8 +46,14 @@
                 </div>
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex-shrink-0 flex items-center">
-                        <img class="block lg:hidden h-10 w-auto" src="https://esquelclic.org/images/logos/LogoCLIC%20.svg" alt="Esquel">
-                        <img class="hidden lg:block h-10 w-auto" src="https://www.esquel.org.ec/templates/g5_hydrogen/custom/images/Logo%20Esquel%20Horizontal.svg" alt="Esquel">
+
+                        <a href="https://www.esquel.org.ec/">
+                            <img src="https://www.esquel.org.ec/templates/g5_hydrogen/custom/images/Logo%20Esquel%20Horizontal.svg" alt="logo_esquel" class="fill-current h-100 w-100 mr-2" width="250" height="250">
+                        </a>
+                        <a href="https://esquelclic.org/">
+                            <img class="fill-current h-100 w-100 mr-2" width="250" height="250" src="https://esquelclic.org/images/logos/LogoCLIC%20.svg" alt="logo_esquel_clic">
+                        </a>
+
                     </div>
                     <div class="hidden sm:block sm:m-8">
                         <div class="flex space-x-4">
@@ -69,10 +75,10 @@
 
     <!--titutlo-->
     <div class=" text-center pb-10 pt-10 ">
-        <h1 class="font-bold text-green-500 break-normal text-3xl md:text-5xl">Mas Cursos Para TI</h1>
+        <h1 class="font-bold text-green-100 break-normal text-3xl md:text-5xl">Mas Cursos Para TI</h1>
     </div>
     <!--tabla-->
-    <div id="General" class="container mx-auto bg-green-500 rounded p-8 text-center overflow-y-auto ">
+    <div id="General" class="container mx-auto bg-green-200 rounded p-8 text-center overflow-y-auto ">
         <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
                 <tr>
@@ -98,7 +104,7 @@
                         <div class="text-sm text-gray-900">25/03/2021</div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-center">
-                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">40 Horas</span>
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-white">40 Horas</span>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-center">
                         <button href="#" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">VER</button>
@@ -108,30 +114,42 @@
             </tbody>
         </table>
     </div>
-    <!--======= footer =========-->
-    <div class=" bg-white shadow shadow">
-        <div class="text-right text-green-300 shadow font-sans text-lg">
-            <div class="flex justify-end  hover:text-white 
-                      hover:bg-green-200">
-                <ul>
-                    <h3 class="font-bold">CONTÁCTANOS</h3>
-                    <li>Av. Colón E4-175 entre Amazonas y Foch, Ed.</li>
-                    <li>Torres de la Colón, Mezzanine Of. 12 Quito - Ecuador</li>
-                    <li>fundacion@esquel.org.ec</li>
-                    <li>+(5932) 252-0001</li>
-                </ul>
-            </div>
-
-        </div>
-    </div>
-    <footer class=" bg-green-200 shadow">
-        <ul class="p-0 py-2 border border-t-2 border-green-200 border-b-0 border-l-0 border-r-0 grid grid-cols-1 md:grid-cols-2 gap-x-0 gap-y-0 md:gap-y-0">
-            <li class="xl:py-0 lg:py-0 md:py-5 sm:py-5 py-2 text-center "><a href="#">© 2021 Fundación Esquel</a></li>
-
-        </ul>
-    </footer>
-    </div>
-
 </body>
 
+    <!--======= footer =========-->
+<footer class="footer bg-green-200 relative pt-1 border-b-2 border-blue-700">
+    <div class="container mx-auto p-6">
+        <div class="sm:flex sm:mt-8">
+            <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+                <div class="flex flex-col">
+
+                    <!--======= Redes sociales =========-->
+                    <span class="font-bold text-center text-gray-700 uppercase mb-2">Area Social</span>
+                    <span class="my-2"><a href="https://www.facebook.com/fundacion.esquel/" class="text-blue-700  text-md hover:text-blue-500">Siguenos en Facebook</a></span>
+                    <span class="my-2"><a href="https://twitter.com/FundacionEsquel" class="text-blue-700  text-md hover:text-blue-500">Siguenos en Twitter</a></span>
+                    <span class="my-2"><a href="https://www.youtube.com/channel/UCS7JrWLBuGoJqhE7ZzNr3Og" class="text-blue-700  text-md hover:text-blue-500">Visítanos en Youtube</a></span>
+                </div>
+                <div class="flex flex-col">
+
+                    <!--======= Contactos =========-->
+                    <span class="font-bold text-center text-gray-700 uppercase mt-4 md:mt-0 mb-2">Contáctanos</span>
+                        <div class="text-right text-white">
+                            <i>Av. Colón E4-175 entre Amazonas y Foch, Ed.</i><br>
+                            <i>Torres de la Colón, Mezzanine Of. 12 Quito - Ecuador</i><br>
+                            <i><a href="fundacion@esquel.org.ec" class="text-blue-700  text-md hover:text-blue-500">fundacion@esquel.org.ec</a></i><br>
+                            <i>+(5932) 252-0001</i><br>
+                        </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-black container mx-auto px-6">
+        <div class="m-0 border-t-2 border-gray-300 flex flex-col items-center">
+            <div class="sm:w-2/3 text-center py-6">
+                <p class="text-sm text-blue-700 font-bold mb-2">© 2021 Fundación Esquel</p>
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
